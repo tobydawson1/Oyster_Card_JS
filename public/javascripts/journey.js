891efd8c5dee168a5b7e1afda524dc241dc4a111
Toby Dawson <tobydawson1@gmail.com>
2020-04-27 13:03:57 +0100

    added entry exit station and zone

--- public/javascripts/journey.js
+++ public/javascripts/journey.js
@@ -2,9 +2,13 @@ function Journey() {
     this.journeyHistory = []
 };
 
-Journey.prototype.entryStation = function(name, zone) {
+Journey.prototype.setEntryStation = function(name, zone) {
     this.entryStation = name
-    this.entryzone = zone
+    this.entryZone = zone
 }
 
+journey.prototype.setExitStation = function(name, zone) {
+    this.exitStation = name
+    this.exitZone = zone
+}
 
