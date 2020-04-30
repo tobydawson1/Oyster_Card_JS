470e0e5499c4e4324248512d9818787a752f9130
Toby Dawson <tobydawson1@gmail.com>
2020-04-30 15:53:47 +0100

    can store multiple journeys

--- spec/javascripts/jasmine_examples/journey_spec.js
+++ spec/javascripts/jasmine_examples/journey_spec.js
@@ -8,10 +8,19 @@ describe("journey", function() {
         it('previous journeys', function() {
             journey.setEntryStation("Stockwell", 2)
             journey.setExitStation("Bank", 1)
-            expect(journey.journeyHistory).toEqual([[["Stockwell", 2], ["Bank", 1]]])
+            journey.setEntryStation("Clapham North", 2)
+            journey.setExitStation("Mordon", 4)
+            journey.setEntryStation("Tooting Bec", 3)
+            journey.setExitStation("Aldgate East", 1)
+            journey.setEntryStation("Oval", 2)
+            journey.setExitStation("Embankment", 1)
+            expect(journey.journeyHistory).toEqual([[["Stockwell", 2], ["Bank", 1]], [["Clapham North", 2], ["Mordon", 4]], [["Tooting Bec", 3], ["Aldgate East", 1]], [["Oval", 2], ["Embankment", 1]]])
         })
+    })
 
-        it('calculates price', function() {
+    describe('can', function() {
+
+        it('calculate price', function() {
             journey.setEntryStation("Stockwell", 2)
             journey.setExitStation("Bank", 1)
             expect(journey.calculatePrice()).toEqual(2)
